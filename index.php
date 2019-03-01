f616dc2fe650c87b8013c90c55561ea5d37ab4f8
John <canadajun1972@gmail.com>
2019-03-01 10:27:22 +0800

    On branch master

---
+++ index.php
@@ -0,0 +1,31 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Download files</title>
+</head>
+<body>
+    
+<form method="post" action="upload.php" enctype="multipart/form-data" >
+<input type="file" name="file">
+<input type="submit" value="Upload">
+
+</form>
+
+<?php
+   
+   $files=scandir('uploads'); 
+    foreach($files as $file){ ?>
+    <p>
+       <a download="<?php echo $file;?>" href="uploads/<?php echo $file;?>"><?php echo $file;?></a>
+    </p>   
+   <?php }
+
+?>
+
+</body>
+</html>
