26d2d788c6d9d6e3945e77fd353e1c035f0fed46
John <canadajun1972@gmail.com>
2019-03-01 12:43:24 +0800

    On branch master

--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php
 
 $file = $_FILES['file']; 
-// save file to download folder
+// save file to uploads foldersfgsdfg
 move_uploaded_file($file['tmp_name'], 'uploads/' . $file['name']);
 
 //redirect to index.php
